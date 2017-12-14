9e42a22ce73494c8eede4b565267bbab5ef0e70b
Felipe de Araujo <me@felipedearaujo.com>
2017-12-14 11:53:44 -0600

    Host fake db on my-json-server.typicode.com

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = { dev: 'http://localhost:9001', prod: 'https://blonyx.herokuapp.com:9001' };
+const API_URL = { dev: 'http://localhost:9001', prod: 'https://my-json-server.typicode.com/felipedaraujo/blognyx-api' };
 const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
 const API_ROOT_URL = API_URL[env];
 
