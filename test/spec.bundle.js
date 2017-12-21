c58be74e7279c3522829790172ce440ceb132bf9
Felipe de Araujo <me@felipedearaujo.com>
2017-12-21 00:13:12 -0600

    Adds enzyme

--- test/spec.bundle.js
+++ test/spec.bundle.js
@@ -3,3 +3,8 @@
 let context = require.context("../src", true, /\.spec\.(js|jsx)$/);
 
 context.keys().forEach(context);
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+Enzyme.configure({ adapter: new Adapter() });
